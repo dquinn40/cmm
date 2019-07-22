e8a29f2b76bbc0d5aed73afd899ee9da035a3261
Dan <dquinn40@gmail.com>
2019-07-22 05:51:48 -0400

    add main application that prints wrapped text for both implementations

--- server/src/main/java/CmmApplication.java
+++ server/src/main/java/CmmApplication.java
@@ -1,5 +1,32 @@
+import java.util.List;
+
 public class CmmApplication {
     public static void main(String[] args) {
+        try {
+            wrapLines();
+            System.out.println("\n\n\n#############################################\n\n\n");
+            wrapAndCondenseParagraphs();
+        } catch (Exception e) {
+            System.out.println("Unable to process file");
+            e.printStackTrace();
+        }
+    }
 
+    private static void wrapLines() throws Exception {
+        List<String> lines = FileReader.readFile("article.txt");
+        List<String> wrappedLines = TextFormatter.wrapText(lines);
+
+        for(String line : wrappedLines) {
+            System.out.println(line);
+        }
+    }
+
+    private static void wrapAndCondenseParagraphs() throws Exception {
+        String content = FileReader.readFileToString("article.txt");
+        List<String> wrappedLines = TextFormatter.condenseAndWrapText(content);
+
+        for(String line : wrappedLines) {
+            System.out.println(line);
+        }
     }
 }
